8a21a5190d154570ce021f20ad960ec006ee76a0
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-08 21:20:53 +0800

    Update binaryAdderSubtractor_4_bit.v

--- Week-5/binaryAdderSubtractor_4_bit.v
+++ Week-5/binaryAdderSubtractor_4_bit.v
@@ -7,8 +7,8 @@ assign W = B ^ M;
 
 fullAdder FA0(S[0], C[1], A[0], W[0], M),
           FA1(S[1], C[2], A[1], W[1], C[1]),
-			 FA2(S[2], C[3], A[2], W[2], C[2]),
-			 FA3(S[3], C4, A[3], W[3], C[3]);
+	  FA2(S[2], C[3], A[2], W[2], C[2]),
+	  FA3(S[3], C4, A[3], W[3], C[3]);
 			 
 assign V = C4 ^ C[3];
 
