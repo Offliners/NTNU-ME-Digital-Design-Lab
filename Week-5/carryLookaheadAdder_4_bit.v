aa30a207884867072db05a85c81491efc9eb66a0
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-08 17:25:36 +0800

    Update carryLookaheadAdder_4_bit.v

--- Week-5/carryLookaheadAdder_4_bit.v
+++ Week-5/carryLookaheadAdder_4_bit.v
@@ -4,15 +4,15 @@ wire [3:0]P, G;
 wire [3:1]C;
 
 halfAdder HA0(P[0], G[0], A[0], B[0]),
-			 HA1(P[1], G[1], A[1], B[1]),
-			 HA2(P[2], G[2], A[2], B[2]),
-			 HA3(P[3], G[3], A[3], B[3]);
+	  HA1(P[1], G[1], A[1], B[1]),
+	  HA2(P[2], G[2], A[2], B[2]),
+	  HA3(P[3], G[3], A[3], B[3]);
 			 
 carryLookaheadGenerator CLG({C4, C}, P, G, C0);
 
 xor G0(S[0], P[0], C0),
-	 G1(S[1], P[1], C[1]),
-	 G2(S[2], P[2], C[2]),
-	 G3(S[3], P[3], C[3]);
+    G1(S[1], P[1], C[1]),
+    G2(S[2], P[2], C[2]),
+    G3(S[3], P[3], C[3]);
 	 
 endmodule
