d63002f384e741a5b419428263e692f8ff435784
Offliner <42777325+Offliners@users.noreply.github.com>
2020-04-10 23:32:12 +0800

    Update TestBench_GL_Dec.v

--- Week-6/TestBench_GL_Dec.v
+++ Week-6/TestBench_GL_Dec.v
@@ -1,15 +1,15 @@
 module TestBench_GL_Dec;
 
 wire [7:0]D;
-reg [2:0]I;
+reg [3:0]I;
 reg [1:0]E;
 
-GL_decoder_3_bit D0(D, I, E);
+GL_decoder_3_bit D0(D, I[2:0], E[0]);
 
 initial 
 begin
 	for(E = 2'b00; E <= 2'b01; E = E + 2'b01)
-		for(I = 3'b000; I <= 3'b110; I = I + 3'b001)
+		for(I = 4'b0000; I <= 4'b0111; I = I + 4'b0001)
 			#50;
 
 end
