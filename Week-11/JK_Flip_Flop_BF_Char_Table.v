c6a90b57e7d2492529362954a2bdaed443993a8a
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-14 22:51:46 +0800

    Update JK_Flip_Flop_BF_Char_Table.v

--- Week-11/JK_Flip_Flop_BF_Char_Table.v
+++ Week-11/JK_Flip_Flop_BF_Char_Table.v
@@ -6,12 +6,12 @@ begin
 		Q <= 1'b0;
 	else
 		begin
-				case ({J, K})
-					2'b00 : Q <= Q;
-					2'b01 : Q <= 1'b0;
-					2'b10 : Q <= 1'b1;
-					2'b11 : Q <= !Q;
-				endcase
+			case ({J, K})
+				2'b00 : Q <= Q;
+				2'b01 : Q <= 1'b0;
+				2'b10 : Q <= 1'b1;
+				2'b11 : Q <= !Q;
+			endcase
 		end
 end
 
