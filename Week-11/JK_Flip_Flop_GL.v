9134b5c5fd9d2a227aac46071ccfc288ec75f678
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-14 22:40:11 +0800

    Update JK_Flip_Flop_GL.v

--- Week-11/JK_Flip_Flop_GL.v
+++ Week-11/JK_Flip_Flop_GL.v
@@ -1,1 +1,13 @@
+module JK_Flip_Flop_GL(output Q, input J, K, clk, rst);
 
+wire [4:0]w;
+
+not G0(w[0], K);
+not G1(w[1], Q);
+and G2(w[2], J, w[1]);
+and G3(w[3], Q, w[0]);
+or G4(w[4], w[2], w[3]);
+
+D_Flip_Flop_behavior DFF(Q, w[4], clk, rst);
+
+endmodule
