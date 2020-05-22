1a1b3983f2a45b188d19acd258a225a0986afb4e
Offliner <42777325+Offliners@users.noreply.github.com>
2020-05-22 11:24:48 +0800

    Update lab10.v

--- Week-12/lab10.v
+++ Week-12/lab10.v
@@ -1,8 +1,10 @@
-module lab10(output [1:0]Y, input E, U, clk, rst);
+module lab10(output [6:0]Y, input E, U, clk, rst);
 
 wire w;
+wire [1:0]Q;
 
 slowclock SC(w, clk);
-two_bit_counter TBC(Y, E, U, w, rst);
+two_bit_counter TBC(Q, E, U, w, rst);
+binary_to_seg7 B(Y, Q);
 
 endmodule
